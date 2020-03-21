27164c3b02b378ca9e67998a1a70171560b3cc03
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-21 12:25:41 +0100

    Commit 7

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-    var source = $('#template-movie').html();
-    var templateMovie = Handlebars.compile(source);
-    $('.click').click(function(){
+    var templateMovieSrc = $('#template-movie').html();
+    var templateMovie = Handlebars.compile(templateMovieSrc);
+
+    $('.click').click(cerca);
+    $('.search-movies').keypress(function (event) {
+        if(event.key == 'Enter') {
+            cerca();
+        }
+    });
+
+
+
+    function cerca(){
         var searchMovies = $('.search-movies').val().toLowerCase();
         //console.log(searchMovies);
         var apiBaseUrl = 'https://api.themoviedb.org/3';
@@ -19,31 +29,39 @@ $(document).ready(function() {
                     for (var i = 0; i < movies.length; i++) {
                         var movie = movies[i];
                         if (movie == 0 ) {
-                            alert('Mi dispiace cerca ancora !')}
-                            $('.search-movies').val('');
+                            alert('Mi dispiace cerca ancora !');
+                        }
+                        //$('.search-movies').val('');
+                        var votoStelle = (movie.vote_average / 2 ).toFixed() ;
+                        var totaleStelle = '';
+                        for (var j = 1; j <=5;  j++) {
+                            if (j <= votoStelle) {
+                                var stella = '<i class="fas fa-star"></i>';
+                            }else {
+                                var stella = '<i class="far fa-star"></i>';
+                            }
+                            totaleStelle += stella;
+                        }
+
+
+
+
+
+
                         var infoMovie = {
                             titolo: movie.title,
                             titoloOriginale: movie.original_title,
                             lingua: movie.original_language,
-                            stelle: movie.vote_average,
-                            //immagineMovies: movie.logo_path
-                        }
+                            voto: totaleStelle,
+                        };
                         var infoMovies = templateMovie(infoMovie);
                         $('.container-movies').append(infoMovies);
                     }
-                    //console.log(infoMovie['stelle']);
-                    var voto = (infoMovie['stelle'] / 2 ).toFixed() ;
-                    console.log(voto);
+                    //console.log(infoMovie);
                 },
                 error: function() {
                     alert('Se non digiti Niente non vedi Niente!!');
                 }
             });
-    });
-
-
-
-
-
-
+    };
 });
