e585fdca6e2f7209d3f7908c360f116ab4e8dfaf
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-23 12:59:51 +0100

    Commit 11

--- js/main.js
+++ js/main.js
@@ -10,15 +10,13 @@ $(document).ready(function() {
     });
     function cerca(){
         var searchMovies = $('.search-movies').val().toLowerCase();
-        //var searchSerie = $('.searchMovies').val().toLowerCase();
         //$('.search-movies').val('');
-        if (searchMovies > 0) {
-            //apiSearch(searchMovies);
+        if (searchMovies.length > 0) {
+            apiSearch(searchMovies);
             // chiamata alla funzione apiSearch
         }
-
-        //------------spostando la funzione apiSearch non sono riuscito a farla funzionare
-    //function apiSearch(searchMovies){
+    }
+    function apiSearch(searchMovies){
         var apiBaseUrl = 'https://api.themoviedb.org/3';
         $.ajax({
                 url: apiBaseUrl + '/search/movie',
@@ -37,25 +35,7 @@ $(document).ready(function() {
                     alert('Se non digiti Niente non vedi Niente!!');
                 }
             });
-    //}
     }
-
-    /*var apiBaseUrl = 'https://api.themoviedb.org/3';
-    $.ajax({
-        url: apiBaseUrl + '/search/tv',
-        method: 'GET',
-        data: {
-            api_key: '37928d22d4c593c96399a7ec5fe9874b',
-            query: searchSerie,
-            language: 'it-IT'
-        },
-        success: function(data) {
-
-        },
-        error:
-
-    });*/
-
     function infoCard(movies) {
         $('.container-movies').empty();
         for (var i = 0; i < movies.length; i++) {
@@ -63,26 +43,34 @@ $(document).ready(function() {
             if (movie == 0 ) {
                 alert('Mi dispiace cerca ancora !');
             }
-            var votoStelle = (movie.vote_average / 2 ).toFixed() ;
-            var totaleStelle = '';
-            for (var j = 1; j <=5;  j++) {
-                if (j <= votoStelle) {
-                    var stella = '<i class="fas fa-star"></i>';
-                }else {
-                    var stella = '<i class="far fa-star"></i>';
-                }
-                totaleStelle += stella;
-            }
+
             var infoMovie = {
                 titolo: movie.title,
                 titoloOriginale: movie.original_title,
                 lingua: movie.original_language,
-                voto: totaleStelle,
+                voto: movie.vote_average,
+                stelle: stars(movie.vote_average)
+
             };
             var infoMovies = templateMovie(infoMovie);
             $('.container-movies').append(infoMovies);
         }
         //console.log(infoMovie);
+
+    }
+
+    function stars(votoStelle) {
+        var votoStelle = Math.ceil(votoStelle / 2) ;
+        var totaleStelle = '';
+        for (var j = 1; j <=5;  j++) {
+            if (j <= votoStelle) {
+                 totaleStelle += '<i class="fas fa-star"></i>';
+            }else {
+                 totaleStelle += '<i class="far fa-star"></i>';
+            }
+
+        }
+        return totaleStelle ;
 
     }
 
